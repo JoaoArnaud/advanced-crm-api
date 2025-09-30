3925811a80f2a54e600455ef92c23b00685969af
João André de Medeiros Arnaud <66128302+JoaoArnaud@users.noreply.github.com>
2025-09-30 17:20:51 -0300

    installing prisma and swagger

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,3 @@ const port: number = 3000;
 app.listen(port, () => {
     console.log(`The API is running on the port: ${port}`)
 });
-
-//DATABASE_URL="postgresql://postgres:1234@localhost:5432/clinica?sslmode=disable"
-//PORT=3333
