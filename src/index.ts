10fcc668e4a18e0aeb670117fef156e6c3616b9f
João André de Medeiros Arnaud <66128302+JoaoArnaud@users.noreply.github.com>
2025-09-30 18:03:27 -0300

    adding tsx for test the api and using dotenv to store the port number

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import { Express }  from "express";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app: Express = express();
-const port: number = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`The API is running on the port: ${port}`)
