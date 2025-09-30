fd827c47a5d5532be31263003411dbeb444b145f
João André de Medeiros Arnaud <66128302+JoaoArnaud@users.noreply.github.com>
2025-09-30 17:17:07 -0300

    testing tsx instead of nodemon and coding the first step of the index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,8 @@ const app: Express = express();
 const port: number = 3000;
 
 app.listen(port, () => {
-    console.log(`A API subiu na porta ${port}`)
+    console.log(`The API is running on the port: ${port}`)
 });
+
+//DATABASE_URL="postgresql://postgres:1234@localhost:5432/clinica?sslmode=disable"
+//PORT=3333
