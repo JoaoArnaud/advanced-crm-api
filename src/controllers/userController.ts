52153c489824d2569659cc52e6790e9b7a9d1f88
João André de Medeiros Arnaud <66128302+JoaoArnaud@users.noreply.github.com>
2025-10-01 14:02:40 -0300

    init controller

---
+++ src/controllers/userController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+import userService from "../services/userService";
+import { User } from "../generated/prisma";
+
+const userController = {
+
+}
+
+export default userController;
